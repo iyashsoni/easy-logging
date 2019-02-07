f1fd3edc19d4ee4ba32c7cb092aba707205d916d
Yash Soni <yasoni12@in.ibm.com>
2019-02-07 15:11:20 +0530

    implemented basic structure

---
+++ Utils.js
@@ -0,0 +1,20 @@
+const _typeCheck = function (type) {
+    switch(type) {
+        case 'name':
+            
+    }
+}
+
+const _mandatoryParam = function (paramName) {
+    return new Error(`${paramName} is a required parameter. Please pass this parameter value while initializing Logger Object.`);
+}
+
+const _existsIn = function (value, container) {
+    if(typeof value == "string" && container.indexOf(value) == -1) {
+        return new Error(`${value} is not a valid value for the field: LogLevel. Please select one of the following: ${container}`);
+    }
+}
+
+export {
+    _mandatoryParam, _typeCheck, _existsIn
+}
